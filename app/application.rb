fb38391dec8e13dd9b6b2b2117a0fb1c67cd8d51
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-30 23:34:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
   def call(env)
     req = Rack::Request.new(env)
     resp = Rack::Response.new
-    
+
         if req.path.match(/items\/.+/)
           item_name = req.path.split("/items/").last
           item = @@items.detect { |i| i.name == item_name }
